e940137f80b8ad1543ffa9e6de59a4f9fc3e80bd
well-Prachi-Jadhav <prachi.jadhav251@pccoepune.org>
2026-07-15 07:16:17 +0530

    Insertion sort

---
+++ Insertion_sort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Insertion_sort {
+    public static void main(String[] args) {
+        int[] arr={5,3,4,1,2};
+        Insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void Insertion(int[] arr ){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j-1]>arr[j]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+      
+}
+
