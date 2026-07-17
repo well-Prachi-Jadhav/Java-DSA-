db95ca1c1881ac1a6e1a576f6231401cd4b774a9
well-Prachi-Jadhav <prachi.jadhav251@pccoepune.org>
2026-07-17 19:26:05 +0530

    Missing Number

--- Missing_noCs.java
+++ Missing_noCs.java
@@ -1,4 +1,37 @@
+public class Missing_noCs {
+    public static void main(String[] args) {
+        int[] arr = {0, 4, 2, 1};
+        System.out.println(findMissing(arr));
+    }
 
-412.        }
+    static int findMissing(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+
+            if (arr[i] == arr.length) {
+                i++;
+            } 
+            else {
+                int correct = arr[i];
+
+                if (arr[i] != arr[correct]) {
+                    int temp = arr[i];
+                    arr[i] = arr[correct];
+                    arr[correct] = temp;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        // Search for missing number
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] != i) {
+                return i;
+            }
+        }
+
+        return arr.length;
     }
 }
